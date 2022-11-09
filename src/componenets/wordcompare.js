83cf8ef95ec437fbcbbb53228b6140bf4fbb2ab2
s3miOtics <>
2022-11-08 17:05:05 -0800

    add keyboard

--- src/componenets/wordcompare.js
+++ src/componenets/wordcompare.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
+
+// components
 import Grid from './Grid'
 import Keypad from './Keypad'
 
-
 export default function Wordle({ solution }) {
   const { currentGuess, guesses, turn, isCorrect, handleKeyup } = useWordle(solution)
-
+  
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
-  
+
     return () => window.removeEventListener('keyup', handleKeyup)
   }, [handleKeyup])
-  
-useEffect(() => {
-  console.log(guesses, turn, isCorrect)
-}, [guesses, turn, isCorrect])
+
+  useEffect(() => {
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
 
   return (
     <div>
       <div>solution - {solution}</div>
       <div>Current Guess - {currentGuess}</div>
       <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
-      <Keypad />
+      {/* <Keypad /> */}
     </div>
   )
 }
