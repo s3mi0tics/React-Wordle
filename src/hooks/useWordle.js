43950cbaf5d9083dcc11d26910e4186681b728d6
s3miOtics <>
2022-11-09 16:52:31 -0800

    add coloring to used keys on keypad

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -9,6 +9,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)]) //each guess is an array
     const [history, setHistory] = useState([]) //each guess is a string
     const [isCorrect, setIsCorrect] = useState(false) 
+    const [usedKeys, setUsedKeys] = useState({}) //{a:'green', b: 'yellow', c: 'grey' }
     
 
     //format a guess into an array of letter objects
@@ -55,6 +56,26 @@ const useWordle = (solution) => {
         setTurn((prevTurn) => {
             return prevTurn + 1
         })
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = { ...prevUsedKeys } 
+
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+                if(l.color === 'green') {
+                newKeys[l.key] = 'green'
+                return
+                }
+                if(l.color === 'yellow' && currentColor!== 'green') {
+                    newKeys[l.key] = 'yellow' 
+                    return
+                }
+                if(l.color === 'grey' && currentColor!== 'yellow' && currentColor!== 'green') {
+                    newKeys[l.key] = 'grey' 
+                    return
+                }
+            })
+            return newKeys
+        })
         setCurrentGuess('')
 
     }
@@ -100,7 +121,7 @@ const useWordle = (solution) => {
         }
     }
 
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
 
 
 }
